f582ae0de3cc4c594866f86874214d43d244da1f
Clayton Green <claytron.g@gmail.com>
2023-03-27 16:20:57 -0700

    init

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,43 @@
+local lsp = require('lsp-zero').preset({
+	name = 'minimal',
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false,
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.setup()
+
+lsp.ensure_installed({
+	"tsserver",
+	"eslint",
+})
+
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-Enter>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+	sign_icons = { }
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+end)
---
+++ lua/cgreen/init.lua
@@ -0,0 +1,4 @@
+require("cgreen.remap")
+require("cgreen.set")
+
+vim.cmd[[colorscheme no-clown-fiesta]]
